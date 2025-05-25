cce13260dbe5384de548a75d38d8821564126094
Sagar2006Krishna <sagar.k1@s.amity.edu>
2025-05-25 15:06:22 +0530

    commiting t1

---
+++ hi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int a, b, sum;
+
+    std::cout << "Enter two numbers: ";
+    std::cin >> a >> b;
+
+    sum = a + b;
+
+    std::cout << "Sum is: " << sum << std::endl;
+
+    return 0;
+}
