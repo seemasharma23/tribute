e4ca804bf71e22dcfbe99eb02de2750fdc046e04
seemasharma23 <seema.sharma1@s.amity.edu>
2025-05-25 15:18:04 +0530

    the changes

--- hi.cpp
+++ hi.cpp
@@ -1,14 +1,16 @@
+//contributed ny SEEMA SHARMA
 #include <iostream>
-
+int add(int x, int y) 
+{
+    return x + y;
+}
 int main() {
-    int a, b, sum;
-
+    int a, b;
     std::cout << "Enter two numbers: ";
-    std::cin >> a >> b;
-
-    sum = a + b;
-
-    std::cout << "Sum is: " << sum << std::endl;
-
+    if (!(std::cin >> a >> b)) {
+        std::cout << "Invalid input!" << std::endl;
+        return 1;
+    }
+    std::cout << "Sum is: " << add(a, b) << std::endl;
     return 0;
 }
